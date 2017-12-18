5d83be82bcb15f6ef79b338160731138c73e7b7b
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-12-18 17:30:22 +0000

    update

--- bin/snli-cli.py
+++ bin/snli-cli.py
@@ -4,6 +4,8 @@ import os
 import time
 import glob
 
+from argparse import ArgumentParser
+
 import torch
 import torch.optim as O
 import torch.nn as nn
@@ -12,8 +14,32 @@ from torchtext import data
 from torchtext import datasets
 
 from snli.model import SNLIClassifier
-from snli.util import get_args, makedirs
+from snli.util import makedirs
 
+
+def get_args():
+    parser = ArgumentParser(description='PyTorch/torchtext SNLI example')
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--d_embed', type=int, default=100)
+    parser.add_argument('--d_proj', type=int, default=300)
+    parser.add_argument('--d_hidden', type=int, default=300)
+    parser.add_argument('--n_layers', type=int, default=1)
+    parser.add_argument('--log_every', type=int, default=50)
+    parser.add_argument('--lr', type=float, default=.001)
+    parser.add_argument('--dev_every', type=int, default=1000)
+    parser.add_argument('--save_every', type=int, default=1000)
+    parser.add_argument('--dp_ratio', type=int, default=0.2)
+    parser.add_argument('--no-bidirectional', action='store_false', dest='birnn')
+    parser.add_argument('--preserve-case', action='store_false', dest='lower')
+    parser.add_argument('--no-projection', action='store_false', dest='projection')
+    parser.add_argument('--train_embed', action='store_false', dest='fix_emb')
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--save_path', type=str, default='results')
+    parser.add_argument('--vector_cache', type=str, default=os.path.join(os.getcwd(), '.vector_cache/input_vectors.pt'))
+    parser.add_argument('--word_vectors', type=str, default='glove.6B.100d')
+    parser.add_argument('--resume_snapshot', type=str, default='')
+    return parser.parse_args()
 
 args = get_args()
 torch.cuda.set_device(args.gpu)
